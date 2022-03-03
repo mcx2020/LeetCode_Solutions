41f249d14ac050b47ef788b76bb699093aaa596b
machaoxue <machaoxue@yeah.net>
2022-03-03 12:15:45 +0800

    update

--- problem-set/1-两数之和.js
+++ problem-set/1-两数之和.js
@@ -24,3 +24,15 @@ const twoSum2 = (nums, target) => {
   return []
 }
 
+const twoSum3 = (nums, target) => {
+  const map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [i, map.get(target - nums[i])]
+    } else {
+      map.set(nums[i], i)
+    }
+  }
+  return []
+}
+
